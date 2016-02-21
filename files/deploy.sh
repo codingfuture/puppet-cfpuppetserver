e7fbce7de06d3e99a0a559036ce556a85cb57416
Andrey Galkin <andrey@futoin.org>
2016-02-21 02:13:36 +0200

    Disabled Puppetfile processing in r10k in favor of librarian & update cfsystem to 0.9.4

--- files/deploy.sh
+++ files/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/opt/puppetlabs/puppet/bin/r10k deploy environment -p
+/opt/puppetlabs/puppet/bin/r10k deploy environment
 
 for penv in /etc/puppetlabs/code/environments/*; do
     pushd $penv
